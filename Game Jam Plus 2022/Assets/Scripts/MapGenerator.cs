a735f801d14a79cffefdec8561e120eca8771a42
Jonas <sena.jonas@outlook.com>
2023-01-27 16:57:06 -0300

    navMesh Fix number 423472384

--- Game Jam Plus 2022/Assets/Scripts/MapGenerator.cs
+++ Game Jam Plus 2022/Assets/Scripts/MapGenerator.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
-
 namespace Game.Map
 {
     
@@ -12,9 +10,13 @@ namespace Game.Map
         [SerializeField] List<Spawn> objectsToSpawn;
         [SerializeField] Vector2Int startPoint; 
         [SerializeField] Vector2Int endPoint;
+        NavMeshPlus.Components.NavMeshSurface navMesh;
+        GameObject navmesh;
         void Start()
         {
+            navmesh = GameObject.Find("NavMesh");
             GridSpawn();
+            navMesh = navmesh.GetComponent<NavMeshPlus.Components.NavMeshSurface>();
         }
 
         void SpawnObject(GameObject _obj, GameObject _parent, Vector2 _pos)
@@ -45,10 +47,7 @@ namespace Game.Map
         // Update is called once per frame
         void Update()
         {
-            //########## QUALQUER PORRA AQUI ->
-
-
-            //
+            navMesh.BuildNavMesh();
             Destroy(this);
         }
     }
